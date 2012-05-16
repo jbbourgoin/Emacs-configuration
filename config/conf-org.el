1813a0940cb89bc9608531e6e63ebb73bcb36a26
Jean-Baptiste Bourgoin <monsieur.camille@gmail.com>
2012-05-16 08:55:49 +0200

    16-05-2012

--- config/conf-org.el
+++ config/conf-org.el
@@ -32,7 +32,7 @@
          :publishing-directory "~/org/public/jbb/"
          :publishing-function org-publish-org-to-html
          :html-preamble ""
-	 :html-postamble "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>."
+	 :html-postamble "<div id=\"licence-text\"><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>.</div>"
 	 :style-include-default nil
 	 )
 	
@@ -43,7 +43,7 @@
          :publishing-function org-publish-org-to-html
          :recursive t
          :html-preamble ""
-	 :html-postamble "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>."
+	 :html-postamble "<div id=\"licence-text\"><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>.</div>"
 	 :auto-sitemap t                ; Generate sitemap.org automagically...
 	 :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
 	 :sitemap-title "Sommaire du journal"         ; ... with title 'Sitemap'.
@@ -57,7 +57,7 @@
          :publishing-function org-publish-org-to-html
          :recursive t
          :html-preamble ""
-	 :html-postamble "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>."
+	 :html-postamble "<div id=\"licence-text\"><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>.</div>"
 	 :auto-sitemap t                ; Generate sitemap.org automagically...
 	 :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
 	 :sitemap-title "Sommaire musique"         ; ... with title 'Sitemap'.
@@ -71,7 +71,7 @@
          :publishing-function org-publish-org-to-html
          :recursive t
          :html-preamble ""
-	 :html-postamble "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>."
+	 :html-postamble "<div id=\"licence-text\"><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\"><img alt=\"Licence Creative Commons\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png\" /></a><br />Ce(tte) oeuvre de <span xmlns:cc=\"http://creativecommons.org/ns#\" property=\"cc:attributionName\">Jean-Baptiste Bourgoin</span> est mise à disposition selon les termes de la <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 non transposé</a>.</div>"
 	 :auto-sitemap t                ; Generate sitemap.org automagically...
 	 :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
 	 :sitemap-title "Sommaire informatique"         ; ... with title 'Sitemap'.
